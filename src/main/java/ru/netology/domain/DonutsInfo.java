909ff77c5147e5cb884a3b83c70ffb32b4dbc66f
Aram Avetisov <aram.avetisov@inbox.ru>
2021-07-06 12:40:53 +0300

    initial commit

---
+++ src/main/java/ru/netology/domain/DonutsInfo.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class DonutsInfo {
+    private boolean isDonut;
+    private int paidDuration;
+    private boolean canPublishFreeCopy;
+    private String editModel;
+    private Placeholder placeholder;
+//+get/set
+}
---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,12 @@
+package ru.netology.manager;
+
+
+public class PostManager {
+    private class Post[] posts();
+    private Post[] search(int ownerId, String domain,String query, int count, int offset, boolean extended, String fields) {
+        return null;
+    }
+    private  void detele(int ownerId, int postId) {
+        //logic
+    }
+
